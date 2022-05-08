34d5b8ab3905aac85941783eedbe0d7061a1b7e5
Vladimir Semeikin <vladimirsemeikinsdsd@gmail.com>
2022-05-08 17:38:48 +0300

    add tab bar controller

---
+++ openNewsProject/openNewsProject/EmailedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmailedViewController.swift
+//  openNewsProject
+//
+//  Created by Developer on 08.05.2022.
+//
+
+import UIKit
+
+class EmailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
